d2f2ddbddef1abc358fb59a7d29075d04ba1b395
李雪姣5 <lixuejiao5@hikvision.com>
2017-12-28 19:55:08 +0800

    OpenLayers2 图层相关demo开发

---
+++ lib/openGis/lib/OpenLayersEx/merge.bat
@@ -0,0 +1,20 @@
+echo /*OpenLayersEx JS Mergre*/ >OpenLayersEx.js
+copy /b OpenLayersEx.js+Base\HtmlIcon.js
+copy /b OpenLayersEx.js+Base\HtmlMarker.js
+copy /b OpenLayersEx.js+Layer\BaiduLayer.js
+copy /b OpenLayersEx.js+Layer\Heatmap.js
+copy /b OpenLayersEx.js+Layer\EzServiceTileLayer.js
+copy /b OpenLayersEx.js+Layer\RootContainerEx.js
+copy /b OpenLayersEx.js+Popup\HikPopup.js
+copy /b OpenLayersEx.js+Popup\DarkPopup.js
+copy /b OpenLayersEx.js+Popup\ShadowPopup.js
+copy /b OpenLayersEx.js+Popup\LabelPopup.js
+copy /b OpenLayersEx.js+Control\ZoomBoxSelect.js
+copy /b OpenLayersEx.js+Control\SelectFeatureEx.js
+copy /b OpenLayersEx.js+Control\EventFeature.js
+copy /b OpenLayersEx.js+Control\LTOverviewMap.js
+copy /b OpenLayersEx.js+Control\HikMeasure.js
+copy /b OpenLayersEx.js+Control\HikPanZoomBar.js
+copy /b OpenLayersEx.js+Control\ZoomAnimation.js
+copy /b OpenLayersEx.js+Util\HikUtil.js
+
